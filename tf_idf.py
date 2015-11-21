794b3e0d21fdcabd5ad75ccd4ca4653b39e1c7f7
inyeoplee77 <blackin77@gmail.com>
2015-11-21 15:26:34 +0900

    tf-idf complete

--- tf_idf.py
+++ tf_idf.py
@@ -4,8 +4,10 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.mllib.feature import HashingTF
 from pyspark.mllib.feature import IDF
-sc = SparkContext()
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 
+sc = SparkContext()
+sqlContext = SQLContext(sc)
 unneeded = [u'Unknown', u'KoreanParticle',u'Hashtag', u'ScreenName' ,u'Number', u'Alpha', u'Foreign',u'Punctuation', u'Suffix', u'Eomi', u'PreEomi' ,u'Josa', u'Exclamation']
 
 def create_wordbag(x):
@@ -21,28 +23,11 @@ def create_wordbag(x):
 		word = text[0]
 		wordbag.append(word)
 	return wordbag
-documents = sc.pickleFile('merged_file').map(lambda x : (x['no'],create_wordbag(x)))
-htf = HashingTF()
-tf_id = documents.mapValues(htf.transform)
-#tf_id.cache()
-#for a in tf_id.take(100):
-#	print a
-#tf = htf.transform(documents.values())
-#tf.cache()
-idf = IDF().fit(tf_id.values())
-#idf.cache()
-
-tfidf_id = idf.transform(tf_id.values())
-#tf_id.mapValues(idf.transform)
-
-print type(tfidf_id)
-
-for a in tfidf_id.take(10):
-	print a
-
-#for vector in tfidf.take(100):
-#	print vector
-#words.saveAsPickleFile('bag_of_words')
-#words = sc.pickleFile('bag_of_words')
-#for word in words.take(100):
-#	print word.encode('utf-8')
+documents = sqlContext.createDataFrame(sc.pickleFile('merged_file').map(lambda x : (x['no'],create_wordbag(x))),['no','words'])
+htf = HashingTF(inputCol= 'words',outputCol = 'rawFeatures')
+featured = htf.transform(documents)
+idf = IDF(inputCol = 'rawFeatures',outputCol = 'idf')
+idfModel = idf.fit(featured)
+tf_idf = idfModel.transform(featured)
+for data in tf_idf.select('no','idf').take(10):
+	print data
